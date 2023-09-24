d220a57dd9c47dbe1b37afbbf169ce73f73a56a8
Keng <kengthao57@gmail.com>
2023-09-24 16:32:17 -0500

    Added code to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,81 @@
-// Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 /* Big letters array */
-const bigLetters = ['A', 'B', 'C', 'D', "E", 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-console.log(bigLetters);
+var bigLettersArr = ['A', 'B', 'C', 'D', "E", 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 /* Small letters array */
-const smallLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-console.log(smallLetters);
+var smallLettersArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 /* Special characters array */
-const specialLetters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '<', '>', '?'];
-console.log(specialLetters);
+var specialLettersArr = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '<', '>', '?'];
 
 /* Numbers array */
-const numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-console.log(numArr);
+var numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-function validateLength(userLength) {
-  return false;
-}
+/* length of password I want it to be */
+var passGeneratorLength = 128;
 
-function generatePassword () {
-  var passlength = prompt("Enter password length 8-128 characters")
-
-    console.log(passlength);
-
-    if (validateLength(passlength)) {
-
-    } else {
-      alert("You have entered an invalid password length")
-      return '';
-    }
-
-    /* does not do anything yet */
-  return newPass;
-};
-
+/* answer array left blank */
+var answerArr = [];
+ 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  /* command takes you to createPrompts to see if it is true. If rightPrompts is true, it will activate the code for generatePassword. */
+  var rightPrompts = createPrompts();
+  /* get selector for the id "password" in index.html */
+  var passwordText = document.querySelector("#password");
+
+  if (rightPrompts) {
+  var newPassword = generatePassword();
+  passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+}
+
+/* Code to generate a password */
+function generatePassword () {
+  var password = "";
+
+  for (var i = 0; i < passGeneratorLength; i++) {
+    /* create a var for a random character generator. Math.floor takes in all of the characters/numbers for the array that I have
+    to create a password */
+    var randomCharacters = Math.floor(Math.random() * answerArr.length);
+    password = password + answerArr[randomCharacters];
+  }
+return password;
+}
+
+/* Question prompts for password generator */
+function createPrompts () {
+  answerArr = [];
+  passGeneratorLength = parseInt(prompt("How many characters do you want for your password? (8 - 128 characters"));
+
+  /* NaN = not a number. */
+  if (isNaN(passGeneratorLength) || passGeneratorLength < 8 || passGeneratorLength > 128) {
+    alert("Password length has to be between 8 - 128. Please enter the number again.");
+    return false; 
+  } 
+
+  if (confirm("Would you like to include upper letters in your password?")) {
+    answerArr = answerArr.concat(bigLettersArr); 
+  }
+
+  if (confirm("Would you like to include lowercase letters in your password?")) {
+    answerArr = answerArr.concat(smallLettersArr); 
+  }
+
+  if (confirm("Would you like to include special letters in your password?")) {
+    answerArr = answerArr.concat(specialLettersArr); 
+  }
+
+  if (confirm("Would you like to include numbers in your password?")) {
+    answerArr = answerArr.concat(numArr); 
+  }
+  return true;
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
+
+I wanted to learn how to use and integrate what I learned in our course into creating an app and using it in a way to create a random password generator for those who needs help creating a really secured password. I learned that creating something like this seems really hard at first but it is a lot simplier than you think when you understand what you are doing and why you are doing it in that certain way.
+
+## Installation
+
+N/A
+
+## Usage
+
+The random password generator can be used to generate a secured and secret password for use. When openning the URL, it will take you to a page. Click on "Generate Password" to generate a password. You will be prompted with multiple questions regarding if you want to add in uppercase/lowercase/etc to your password. At the end of the prompts, it will generate a password depending on your answers to the prompts. This was created through mostly using Javascript to help build a password.
+
+## Credits
+https://git.bootcampcontent.com/University-of-Wisconsin-Extended-Campus/UofW-VIRT-FSF-PT-08-2023-U-LOLC/-/tree/main/03-JavaScript/01-Activities - Gitlab activities
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN - Mozilla
+https://www.youtube.com/watch?v=v2jfGo7ztm8 - Youtube video
+
+## License
+
+No License
+
+
+
